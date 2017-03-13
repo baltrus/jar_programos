9bd59d6aabb3422f8e2fb934c42990f2028c6694
baltrus <bobexass@gmail.com>
2017-03-13 21:29:40 +0200

    duombaziu nuskaitymas3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import duombazes_nuskaitymas.Duombazes_nuskaitymas;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+Duombazes_nuskaitymas objektas = new Duombazes_nuskaitymas();
+objektas.Pasisveikinimas();
+objektas.pasirinkimas();
+objektas.Vykdymas();
     }
 }
+
