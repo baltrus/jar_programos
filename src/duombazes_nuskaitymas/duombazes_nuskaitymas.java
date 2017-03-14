13b5083ca442f5d4200397e93e301d6c95f91017
baltrus <bobexass@gmail.com>
2017-03-14 20:43:11 +0200

    duombaziu nuskaitymas 5

--- src/duombazes_nuskaitymas/duombazes_nuskaitymas.java
+++ src/duombazes_nuskaitymas/duombazes_nuskaitymas.java
@@ -1,11 +1,29 @@
 package duombazes_nuskaitymas;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
 import java.util.Scanner;
 
 /**
  * Created by Baltramiejus Jakstys on 3/13/2017.
  */
 public class Duombazes_nuskaitymas {
+    private Connection connection;
+
+    public Duombazes_nuskaitymas() {
+
+    }
+
+    void Duombazes_nuskaitymas() {
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql:localhost:3306/ksc",
+                    "root",
+                    ""
+            );
+        } catch (Exception error) {
+        }
+    }
 
     public void Pasisveikinimas() {
         System.out.println("Sveiki, dirbsite su duomenų bazėje esančiomis lentelėmis.");
@@ -33,14 +51,52 @@ public class Duombazes_nuskaitymas {
     }
 
     public void Studentu_lentele() {
-        System.out.println("jus ivedete 1");
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM `students`.`id`, `students`.`name`, `students`.`surname`");
+            while (resultSet.next()) {
+                System.out.print(resultSet.getInt("id"));
+                System.out.print("|");
+                System.out.print(resultSet.getString("name"));
+                System.out.print("|");
+                System.out.print(resultSet.getInt("surname"));
+                System.out.print("");
+            }
+        } catch (Exception error) {
+            System.out.println(error);
+        }
     }
 
     public void Studentu_adresu_lentele() {
-        System.out.println("jus ivedete 2");
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM `student_address`.`student_id`, `student_address`.`street`, `student_address`.`city`, `student_address`.`country`");
+            while (resultSet.next()) {
+                System.out.print(resultSet.getInt("id"));
+                System.out.print("|");
+                System.out.print(resultSet.getString("street"));
+                System.out.print("|");
+                System.out.print(resultSet.getString("city"));
+                System.out.print("");
+            }
+        } catch (Exception error) {
+            System.out.println(error);
+        }
     }
 
     public void Studentu_pazymiu_lentele() {
-        System.out.println("jus ivedete 3");
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM 'student_marks'.`student_id`, 'student_marks'.`title`, 'student_marks'.`mark`");
+            while (resultSet.next()) {
+                System.out.print(resultSet.getInt("id"));
+                System.out.print("|");
+                System.out.print(resultSet.getString("title"));
+                System.out.print("|");
+                System.out.print(resultSet.getInt("mark"));
+            }
+        } catch (Exception error) {
+            System.out.println(error);
+        }
     }
 }
